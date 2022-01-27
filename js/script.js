8874453991a3f749de152059da8dd73314bd0b05
VostochnyyLev <sasha.yaremenko.00@gmail.com>
2022-01-27 20:21:04 +0200

    add package json

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 fetch(
-  "http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no"
+  "https://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no"
 )
   .then((response) => response.json())
   .then((data) => render(data));
