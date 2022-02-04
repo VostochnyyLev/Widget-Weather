b6b289711d3e095ee084037afa0163b3eb3d4c9d
VostochnyyLev <sasha.yaremenko.00@gmail.com>
2022-02-04 23:14:11 +0200

    add webpack and create modules

--- src/render.js
+++ src/render.js
@@ -1,18 +1,12 @@
-function render(data) {
+export default function render(data) {
   document.getElementById('wrapper').innerHTML = `
   <div> Country: ${data.location.country}; </div>
     <div> City: ${data.location.name}; </div>
     <div>Time:${data.location.localtime}; </div>
     <div>Temperature: ${data.current.temp_c};</div>
     <div>${data.current.condition.text};</div>
-    <div>  
+    <div>
       <img src=${data.current.condition.icon} alt="">
     </div>
     `;
 }
-
-fetch(
-  'https://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no',
-)
-  .then((response) => response.json())
-  .then((data) => render(data));
